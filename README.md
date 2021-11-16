5bb56978841cedb6806f6954895f4a1264ac8a1b
mariajbellido <mariajbellido@gmail.com>
2021-11-16 16:04:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# little-piano-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/little-piano-app)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+ 
+})
+export class AppComponent  {
+  title= "Piano";
+
+  aplicarSonido(numero: number):void {
+    const audio = new Audio();
+    audio.src = "./assets/sonidos/note" + numero + ".wav";
+    audio.load();
+    audio.play();
+  }
+  
+}
